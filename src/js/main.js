ccd3bd982c72886edc4c657ce1cdc72d96e1fe30
unknown <dianagavrichenko091020@gmail.com>
2023-10-27 17:15:59 +0300

    timur

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 import'../scss/style.scss';
+
+const openMenuButton = document.querySelector('.main-header__button');
+const Menu = document.querySelector('.main-header__nav');
+
+openMenuButton.addEventListener('click',() => {
+    menu.classList.add('open');
+});
